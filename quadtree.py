2b921439e10656ae1c00bbe820dc6c2068819329
XMaceska <56258818+XMaceska@users.noreply.github.com>
2019-12-21 16:37:29 +0100

    upraven výpočet vzdálenosti

--- quadtree.py
+++ quadtree.py
@@ -34,3 +34,10 @@ def bounding_box(list):
     bouding_box_list = [[maxcoords[0],maxcoords[1],mincoords[0],mincoords[1]]]
     mid_x = (maxcoords[0] + mincoords[0])/2
     mid_y = (maxcoords[1] + mincoords[1])/2
+    len_x = abs(maxcoords[0] - mincoords[0]) / 2
+    len_y = abs(maxcoords[1] - mincoords[1]) / 2
+    print(mid_x, mid_y)
+    return bouding_box_list, mid_x, mid_y, len_x, len_y
+
+
+bounding_box(coordinates_D)
