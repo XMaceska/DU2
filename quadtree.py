e533278dee7bf093714bf1c9302a5547694b18d4
XMaceska <56258818+XMaceska@users.noreply.github.com>
2019-12-21 16:38:01 +0100

    přidána funkce quadtree

--- quadtree.py
+++ quadtree.py
@@ -41,3 +41,56 @@ def bounding_box(list):
 
 
 bounding_box(coordinates_D)
+
+
+def quadtree (json_data,x_mid,y_mid,len_y, len_x, id,quad = 0):
+    """quad-tree function, recursive"""
+    new_json = []
+    NW = []
+    NE = []
+    SW = []
+    SE = []
+
+    if len(json_data) < 50:
+        print("ok")
+        new_id = id[0]
+        for i in json_data:
+            i["properties"]["cluster_id"] = new_id
+            new_json.append(i)
+            print(new_json)
+            id.append(new_id + 1)
+    if quad == 1:
+        x_mid = x_mid - len_x*2
+        y_mid = y_mid + len_y
+
+    elif quad == 2:
+        x_mid = x_mid + len_x*2
+        y_mid = y_mid + len_y
+
+    elif quad == 3:
+        x_mid = x_mid - len_x*2
+        y_mid = y_mid - len_y
+
+    elif quad == 4:
+        x_mid = x_mid + len_x*2
+        y_mid = y_mid - len_y
+
+    for u in json_data:
+        x, y = u[coordinates_data]
+
+        if x < x_mid and y > y_mid:
+            NW.append(u)
+        elif x > x_mid and y > y_mid:
+            NE.append(u)
+        elif x < x_mid and y < y_mid:
+            SW.append(u)
+        elif x < x_mid and y < y_mid:
+            SE.append(u)
+        else:
+            print("out")
+    quadtree(NW,x_mid, y_mid, len_x, len_y, id, quad = 1)
+    quadtree(NE,x_mid, y_mid, len_x, len_y, id, quad = 2)
+    quadtree(SW,x_mid, y_mid, len_x, len_y, id, quad = 3)
+    quadtree(SE,x_mid, y_mid, len_x, len_y, id, quad = 4)
+
+    return new_json,id
