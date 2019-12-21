65022d0e99d5c1c917cc74ee0e74f02f216c369d
XMaceska <56258818+XMaceska@users.noreply.github.com>
2019-12-21 16:36:39 +0100

    Funkce pro zobrazení souřasdnic + min, max

--- quadtree.py
+++ quadtree.py
@@ -4,16 +4,33 @@ datalist = []
 ID_cluster = 0
 
 
-def divide(data, north, south, east, west):
-    """ this function divides cluster only if there is more than 50 point is cluster """
-    """ recursion function"""
-    if len(data) < 5:
-        global datalist
-        global ID_cluster
-        for i in data:
-            datalist = [(i[0] , i[1])] = ID_cluster
-        ID_cluster = +1
-        return data
-    else:
+def show_coordinates(json_data):
+    """loads coordinates from GEOjson into list"""
+    global coordinates_data
+    coordinates_list = []
+    for x in json_data["features"]:
+        coordinates_data = (x["geometry"]["coordinates"])
+        coordinates_list.append(coordinates_data)
+    print(coordinates_list)
+    return(coordinates_list)
 
 
+coordinates_D = show_coordinates(data)
+
+
+
+def bounding_box(list):
+    """find max and min in coordinates data"""
+    global mid_x
+    global mid_y
+    global len_x
+    global len_y
+    maxcoords = max(list)
+    print("max x:", maxcoords[0])
+    print("max y:", maxcoords[1])
+    mincoords = min(list)
+    print("min x:", mincoords[0])
+    print("min y:", mincoords[1])
+    bouding_box_list = [[maxcoords[0],maxcoords[1],mincoords[0],mincoords[1]]]
+    mid_x = (maxcoords[0] + mincoords[0])/2
+    mid_y = (maxcoords[1] + mincoords[1])/2
